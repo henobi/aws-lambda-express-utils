39d343588bef86825170ef2c3f6e51e913d407ae
Hendrik Obermayer <hendrik.obermayer@mhp.com>
2021-10-07 17:47:34 +0200

    Added tests to cognito middleware

--- src/cognito/__tests__/index.test.ts
+++ src/cognito/__tests__/index.test.ts
@@ -22,4 +22,57 @@ test('Check cognito middleware with no user', () => {
   expect(next).toBeCalledTimes(0);
 });
 
-// TODO: Check middleware with mock user
+test('Check cognito middleware with user 1', () => {
+  const myCognitoMiddleware = cognitoMiddleware({ userpoolId: 'abc12345' });
+  expect(myCognitoMiddleware).toBeTruthy();
+
+  const next = jest.fn();
+  const req = {
+    apiGateway: {
+      event: {
+        requestContext: {
+          authorizer: {
+            claims: {
+              sub: '134',
+            },
+          },
+        },
+      },
+    },
+    user: undefined,
+  };
+
+  const res = { header: jest.fn(), status: jest.fn().mockReturnThis(), json: jest.fn() };
+
+  myCognitoMiddleware(req, res, next);
+  expect(req.user).toBeTruthy();
+  expect(res.status).toBeCalledTimes(0);
+  expect(next).toBeCalledTimes(1);
+});
+
+test('Check cognito middleware with user 2', () => {
+  const myCognitoMiddleware = cognitoMiddleware({ userpoolId: 'abc12345' });
+  expect(myCognitoMiddleware).toBeTruthy();
+
+  const next = jest.fn();
+
+  const req = {
+    apiGateway: {
+      event: {
+        requestContext: {
+          identity: {
+            cognitoAuthenticationProvider: ':CognitoSignIn:abc12345',
+          },
+        },
+      },
+    },
+    user: undefined,
+  };
+
+  const res = { header: jest.fn(), status: jest.fn().mockReturnThis(), json: jest.fn() };
+
+  myCognitoMiddleware(req, res, next);
+  expect(req.user).toBeTruthy();
+  expect(res.status).toBeCalledTimes(0);
+  expect(next).toBeCalledTimes(1);
+});
